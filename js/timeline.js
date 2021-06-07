67ebea94be0152b33f9c9c604bbf1447a1a22be4
marlonhanks <owner@kedia.co>
2021-06-07 01:08:02 +0000

    update timeline

---
+++ js/timeline.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+	var my_posts = $("[rel=tooltip]");
+
+	var size = $(window).width();
+	for(i=0;i<my_posts.length;i++){
+		the_post = $(my_posts[i]);
+
+		if(the_post.hasClass('invert') && size >=767 ){
+			the_post.tooltip({ placement: 'left'});
+			the_post.css("cursor","pointer");
+		}else{
+			the_post.tooltip({ placement: 'rigth'});
+			the_post.css("cursor","pointer");
+		}
+	}
+});
+
+$(function(){
+  var btn_share = $("[rel=modalShare]");
+  
+  btn_share.click(function(e){
+    e.preventDefault();
+    $("#myModal").modal('show');
+  });
+});
